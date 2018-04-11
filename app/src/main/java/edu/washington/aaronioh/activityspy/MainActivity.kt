df3b3f3ba5b0e7764801f3255484e00052c26459
Aaron Oh <aaronioh@D-69-91-185-149.dhcp4.washington.edu>
2018-04-11 01:12:56 -0700

    app

---
+++ app/src/main/java/edu/washington/aaronioh/activityspy/MainActivity.kt
@@ -0,0 +1,55 @@
+package edu.washington.aaronioh.activityspy
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG = "MainActivity"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.i(TAG, "onCreate event fired")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.i(TAG, "onStart event fired")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.i(TAG, "onResume event fired")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.i(TAG, "onPause event fired")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.i(TAG, "onStop event fired")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        Log.i(TAG, "onRestart event fired")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.e(TAG, "We’re going down, Captain!")
+    }
+}
